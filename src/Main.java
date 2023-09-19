dd22233a9331da8196c1820d777ce10bdf9a8bda
userKim96 <rlaclgnsska002@gmail.com>
2023-09-19 19:29:45 +0900

    게시물 작성 기능 추가

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,14 @@
 import java.util.Scanner;
 
 public class Main {
+//	필드
 	public static void main(String[] args) {
-		
+//		메소드
 		System.out.println("====프로그램 실행====");
+		
 		Scanner sc = new Scanner(System.in);
+		
+		int lastArticleId = 0;
 		
 		while (true) {
 			
@@ -14,6 +18,23 @@ public class Main {
 			
 			if (commend.length() == 0) {
 				System.out.println("명령어를 입력하세요.");
+			}
+			
+			
+			if (commend.equals("article write")) {
+				
+				int id = lastArticleId+1;
+				
+				System.out.println("제목 : ");
+				String title = sc.nextLine();
+				
+				System.out.println("내용 : ");
+				String body = sc.nextLine();
+				
+				System.out.printf("%d번 글이 생성되었습니다.\n", id);
+				
+				lastArticleId++;
+				
 			}
 			
 			else {
@@ -32,5 +53,6 @@ public class Main {
 		sc.close();
 	}
 }
-	
+
+
 
