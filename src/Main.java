7b5d77f63b687bcd3b6e94ed964de4179a3e8b90
userKim96 <rlaclgnsska002@gmail.com>
2023-09-19 20:12:01 +0900

    게시물 리스트 기능 추가

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 //	필드
 	public static void main(String[] args) {
 //		메소드
+		
+		
 		System.out.println("====프로그램 실행====");
 		
 		Scanner sc = new Scanner(System.in);
 		
+		List<Article> articles = new ArrayList<Article>();
+
 		int lastArticleId = 0;
 		
 		while (true) {
@@ -18,6 +24,7 @@ public class Main {
 			
 			if (commend.length() == 0) {
 				System.out.println("명령어를 입력하세요.");
+				continue;
 			}
 			
 			
@@ -33,12 +40,36 @@ public class Main {
 				
 				System.out.printf("%d번 글이 생성되었습니다.\n", id);
 				
+				Article article = new Article(id, title, body);
+				
+				articles.add(article);
+						
 				lastArticleId++;
 				
 			}
 			
+			else if (commend.equals("article list")) {
+				
+				if (articles.size() == 0) {
+					
+					System.out.println("게시물이 없습니다.");
+					
+					
+				}
+				else if(articles.size() > 0) {
+					
+					System.out.println("번호   /   제목");
+					
+					for (int i = articles.size()-1; i >= 0; i--) {
+						Article article = articles.get(i);
+						System.out.printf("%d   /   %s\n", article.id, article.title);
+					}
+				}
+			}
+			
 			else {
 				System.out.println("없는 명령어 입니다.");
+				continue;
 			}
 			
 			if (commend.equals("exit")) {
@@ -54,5 +85,16 @@ public class Main {
 	}
 }
 
-
+class Article {
+	
+	int id;
+	String title;
+	String body;
+	
+	Article(int id, String title, String body) {
+		this.id = id;
+		this.title = title;
+		this.body = body;
+	}
+}
 
